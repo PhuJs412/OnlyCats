8ec9dfc103a313643ebe4eda0cd00629f9c82c82
Doan Minh Phu <phujason1992@gmail.com>
2025-07-14 07:01:50 +0700

    feat: adding errorHandle and notFound middleware to handle error

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './routes';
+import bodyParser from 'body-parser'; // chưa biết để làm gì 
+import { notFound, errorHandler } from './middleware/handle_error.middleware';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use('/onlycats', router);
 
-app.get('/', (req, res) => {
-    res.send('API is running ...');
-});
+//Xử lý cuối cùng
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
---
+++ src/middleware/authenJWT.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export interface AuthRequest extends Request {
+    user?: { id: string; email: string };
+}
+
+export const authenticateJWT: RequestHandler = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        res.status(401).json({ message: 'Unauthorized - No token provided' });
+        return;
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+            id: string;
+            email: string;
+        };
+
+        // ép lại kiểu cho req nếu cần
+        (req as AuthRequest).user = { id: decoded.id, email: decoded.email };
+
+        next();
+    } catch (err) {
+        res.status(403).json({ message: 'Invalid token' });
+        return;
+    }
+};
