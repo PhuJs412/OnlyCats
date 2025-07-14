d001faee0cf3479127ab9463f8c9da1ad92d6d7f
Doan Minh Phu <phujason1992@gmail.com>
2025-07-14 07:16:07 +0700

    feat: adding enums for handle input values

---
+++ src/utils/enums.ts
@@ -0,0 +1,19 @@
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+}
+
+export enum Visibility {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+  FOLLOWER_ONLY = 'follower_only'
+}
+
+export enum ReactionType {
+  LIKE = 'like',
+  SAD = 'sad',
+  ANGRY = 'angry',
+  WOW = 'wow',
+  CARE = 'care',
+}
