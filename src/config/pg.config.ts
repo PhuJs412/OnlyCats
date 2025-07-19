054f29cf58f9324e8b6117614b158adc1e15aa23
Doan Minh Phu <phujason1992@gmail.com>
2025-07-19 22:42:05 +0700

    fix: adding nodemailer for otp sending code + upload db on cloud

--- src/config/pg.config.ts
+++ src/config/pg.config.ts
@@ -4,17 +4,27 @@ import dotenv from 'dotenv';
 dotenv.config(); //Thực thi hàm config() của Pool trong pg => để sử dụng cơ chế đọc file .env và lấy giá trị từ đó
 
 export const pool = new Pool({
-    host: process.env.DB_HOST || '',
-    port: Number(process.env.DB_PORT) || 0, // tham số port là 1 number => cần ép kiểu về
-    database: process.env.DB_NAME || '',
-    user: process.env.DB_USER || '',
-    password: String( process.env.DB_PASSWORD) || ''
+
+    //DÀNH CHO KẾT NỐI TỪ NEON (DB CLOUD)
+
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+
+    //DÀNH CHO KẾT NỐI THỦ CÔNG
+
+    // host: process.env.DB_HOST || '',
+    // port: Number(process.env.DB_PORT) || 0,
+    // database: process.env.DB_NAME || '',
+    // user: process.env.DB_USER || '',
+    // password: String(process.env.DB_PASSWORD) || ''
 });
 
-export async function testDbConnection(){
+export async function testDbConnection() {
     try {
-        const res = await pool.query('SELECT NOW()'); //Lấy thời gian hiện tại
-        console.log('Connected db successfully at: ', res.rows[0].now);
+        const time = await pool.query('SELECT NOW()');
+        console.log('Connected db successfully at: ', time.rows[0].now);
     } catch (error) {
         console.error('Db connection failed !');
         console.log('Error: ', error);
