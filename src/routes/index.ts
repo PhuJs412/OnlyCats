6020059f8440872e6b5648d03f8e9d1d61ee150a
Doan Minh Phu <phujason1992@gmail.com>
2025-07-20 12:44:57 +0700

    feat: adding otp sending api + lib nodemailer supporting to send mail

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import userRoute from './user.routes';
 import postRoute from './post.routes';
 import commentRoute from './comment.route';
 import followRoute from './follow.routes';
-
+import otpRoute from './otp.routes';
 const router = Router();
 
 router.use('/authen', authRoute);
@@ -12,5 +12,6 @@ router.use('/user', userRoute);
 router.use('/post', postRoute);
 router.use('/comment', commentRoute);
 router.use('/follow', followRoute);
+router.use('/otp', otpRoute);
 
 export default router;
