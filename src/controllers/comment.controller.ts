605ad9af8ab026c5ad8ffdc13ef236c298035d0f
Phu Dep Trai <phujason1992@gmail.com>
2025-07-21 20:53:25 +0700

    feat: enhance comment functionality with authentication and notification system

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as commentService from '../services/comment.service';
 import { CommentUpdate } from '../models/comment.update.model';
+import { AuthRequest } from '../middleware/authenJWT.middleware';
 
 export const getAllCommentByPostId = async (req: Request, res: Response) => {
     try {
@@ -29,15 +30,6 @@ export const getRepliesByCommentId = async (req: Request, res: Response) => {
     }
 };
 
-export const getReplyByCommentId = async (req: Request, res: Response) => {
-    try {
-        const comments = await commentService.getReplyByCommentId(req.params.comment_id);
-        res.status(200).json(comments);
-    } catch (error) {
-        res.status(500).json({ message: (error as Error).message });
-    }
-};
-
 export const countCommentByPostId = async (req: Request, res: Response) => {
     try {
         const commentCount = await commentService.countCommentByPostId(req.params.post_id);
@@ -56,20 +48,23 @@ export const countReplyByCommentId = async (req: Request, res: Response) => {
     }
 };
 
-export const createComment = async (req: Request, res: Response) => {
+export const createComment = async (req: AuthRequest, res: Response) => {
     try {
-        const { user_id, post_id, content } = req.body;
-        await commentService.createComment(user_id, post_id, content);
+        const loginUserId = req.user?.id || '';
+        const { post_id, content } = req.body;
+        console.log('abc: ', req.user?.id)
+        await commentService.createComment(loginUserId, post_id, content);
         res.status(200).json('ok');
     } catch (error) {
         res.status(500).json({ message: (error as Error).message });
     }
 };
 
-export const createReply = async (req: Request, res: Response) => {
+export const createReply = async (req: AuthRequest, res: Response) => {
     try {
-        const { user_id, post_id, content, parent_comment_id } = req.body;
-        await commentService.createReply(user_id, post_id, content, parent_comment_id);
+        const loginuserId = req.user?.id || '';
+        const { post_id, content, parent_comment_id } = req.body;
+        await commentService.createReply(loginuserId, post_id, content, parent_comment_id);
         res.status(200).json('ok');
     } catch (error) {
         res.status(500).json({ message: (error as Error).message });
--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,11 +1,11 @@
 export interface Comments {
-    id: string;
-    user_id: string;
-    post_id: string;
+    id?: string;
+    user_id?: string;
+    post_id?: string;
     parent_comment_id?: string | null;
-    content: string;
-    is_edited: boolean;
-    is_deleted: boolean;
-    created_at: string;   // ISO date string
-    updated_at: string;   // ISO date string
+    content?: string;
+    is_edited?: boolean;
+    is_deleted?: boolean;
+    created_at?: string;   // ISO date string
+    updated_at?: string;   // ISO date string
 };
